f55b0c2553cd13ebfb499f93e7c353465d860944
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-06-28 08:00:40 +0100

    changed field id in Tweet class to deserialize id_str in tweets.js

--- src/main/java/com/olujobii/model/Tweet.java
+++ src/main/java/com/olujobii/model/Tweet.java
@@ -1,6 +1,9 @@
 package com.olujobii.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public record Tweet(
+        @SerializedName("id_str")
         String id,
         String full_text
 ) {
