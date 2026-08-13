1b890f47b195617bfb1a51df88b1d7102c441662
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-08-13 19:43:43 +0100

    test resources

---
+++ src/test/resources/malformed-tweets.js
@@ -0,0 +1,257 @@
+window.YTD.tweets.part0 = 
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1723456789012345601"
+          ],
+          "editableUntil" : "2026-03-15T14:22:10.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Sample User One",
+            "screen_name" : "sampleuser01",
+            "indices" : [
+              "0",
+              "13"
+            ],
+            "id_str" : "2001111222333444555",
+            "id" : "2001111222333444555"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "45"
+      ],
+      "favorite_count" : "3",
+      "in_reply_to_status_id_str" : "1723456789012340000",
+      "id_str" : "1723456789012345601",
+      "in_reply_to_user_id" : "2001111222333444555",
+      "truncated" : false,
+      "retweet_count" : "1",
+      "id" : "1723456789012345601",
+      "in_reply_to_status_id" : "1723456789012340000",
+      "created_at" : "Sun Mar 15 13:22:10 +0000 2026",
+      "favorited" : false,
+      "full_text" : "@sampleuser01 that's actually hilarious omg😂",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "sampleuser01",
+      "in_reply_to_user_id_str" : "2001111222333444555"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1723456789012345602"
+          ],
+          "editableUntil" : "2026-02-08T09:47:33.000Z",
+          "editsRemaining" : "4",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Naija Foodies Hub",
+            "screen_name" : "NaijaFoodiesHub",
+            "indices" : [
+              "0",
+              "16"
+            ],
+            "id_str" : "2002222333444555666",
+            "id" : "2002222333444555666"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "38"
+      ],
+      "favorite_count" : "7",
+      "in_reply_to_status_id_str" : "1723456789012340111",
+      "id_str" : "1723456789012345602",
+      "in_reply_to_user_id" : "2002222333444555666",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1723456789012345602",
+      "in_reply_to_status_id" : "1723456789012340111",
+      "created_at" : "Sun Feb 08 08:47:33 +0000 2026",
+      "favorited" : true,
+      "full_text" : "@NaijaFoodiesHub abeg no vex, e sweet die🔥",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "NaijaFoodiesHub",
+      "in_reply_to_user_id_str" : "2002222333444555666"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1723456789012345603"
+          ],
+          "editableUntil" : "2026-01-21T18:05:59.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Dami Codes",
+            "screen_name" : "damicodes",
+            "indices" : [
+              "0",
+              "10"
+            ],
+            "id_str" : "2003333444555666777",
+            "id" : "2003333444555666777"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "29"
+      ],
+      "favorite_count" : "2",
+      "in_reply_to_status_id_str" : "1723456789012340222",
+      "id_str" : "1723456789012345603",
+      "in_reply_to_user_id" : "2003333444555666777",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1723456789012345603",
+      "in_reply_to_status_id" : "1723456789012340222",
+      "created_at" : "Wed Jan 21 17:05:59 +0000 2026",
+      "favorited" : false,
+      "full_text" : "@damicodes make we see am first😂",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "damicodes",
+      "in_reply_to_user_id_str" : "2003333444555666777"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1723456789012345604"
+          ],
+          "editableUntil" : "2025-12-05T11:12:45.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Chika Reviews",
+            "screen_name" : "ChikaReviews",
+            "indices" : [
+              "0",
+              "13"
+            ],
+            "id_str" : "2004444555666777888",
+            "id" : "2004444555666777888"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "41"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1723456789012340333",
+      "id_str" : "1723456789012345604",
+      "in_reply_to_user_id" : "2004444555666777888",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1723456789012345604",
+      "in_reply_to_status_id" : "1723456789012340333",
+      "created_at" : "Fri Dec 05 10:12:45 +0000 2025",
+      "favorited" : false,
+      "full_text" : "@ChikaReviews haba, no be small thing at all😭",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "ChikaReviews",
+      "in_reply_to_user_id_str" : "2004444555666777888"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1723456789012345605"
+          ],
+          "editableUntil" : "2025-09-27T21:38:02.000Z",
+          "editsRemaining" : "3",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Tunde Sports Talk",
+            "screen_name" : "TundeSportsTalk",
+            "indices" : [
+              "0",
+              "16"
+            ],
+            "id_str" : "2005555666777888999",
+            "id" : "2005555666777888999"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "36"
+      ],
+      "favorite_count" : "5",
+      "in_reply_to_status_id_str" : "1723456789012340444",
+      "id_str" : "1723456789012345605",
+      "in_reply_to_user_id" : "2005555666777888999",
+      "truncated" : false,
+      "retweet_count" : "2",
+      "id" : "1723456789012345605",
+      "in_reply_to_status_id" : "1723456789012340444",
+      "created_at" : "Sat Sep 27 20:38:02 +0000 2025",
+      "favorited" : true,
+      "full_text" : "@TundeSportsTalk that match was pure madness⚽😂",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "TundeSportsTalk",
+      "in_reply_to_user_id_str" : "2005555666777888999"
+    }
+  }
+]
